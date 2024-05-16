7ebc8dade100dbb83e0daded7adfa27cfeba0bd7
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-05-16 10:23:00 -0600

    keep origins to *

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,6 @@ from routes import authorization, exercises, users, workouts, workout_exercises,
 app = FastAPI()
 
 origins = ["*", "http://localhost:3000"]
-# "https://gym-app-frontend-p21x.onrender.com"
 
 app.add_middleware(
     CORSMiddleware,
